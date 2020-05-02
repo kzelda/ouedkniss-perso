b2574413d9667465db0a1394c975d87f2aca01f7
kzelda <kzelda@users.noreply.github.com>
2020-05-02 09:43:51 +0200

    remove jquery

--- ouedkniss.css.user.js
+++ ouedkniss.css.user.js
@@ -9,60 +9,81 @@
 // ==/UserScript==
 
 
-if(window.location.href.indexOf('/membre/') == -1)
-{
-try{ document.getElementById("page_left").style.paddingRight = "0px"; }catch(e){}
-  
-$(".bouton_details , .annonce_left a").attr("target","_blank");
+(function(){
 
-try{document.querySelector("#page_right").remove();}catch(e){}
+if (window.location.href.indexOf('/membre/') > -1) return;
 
-$("body").css("padding-top" , "46px");
+var body = document.querySelector("body");
+var menu = document.getElementById("menu");
+var find = (selector) => document.querySelectorAll(selector);
+
+var add_style = function () {
+
+	// create custom css style
+	var mystyle = document.createElement("style");
+	mystyle.id = "mystyle";
+	body.append(mystyle);
+	body.append(menu);
+
+	mystyle.innerText = `
+
+	#banner {
+	 display: none
+	}
+
+	.body_banner {
+	  padding-top: 0!important;
+	}
+
+	#header {
+	  display:none
+	}
+
+	#topbar {
+	  display: none
+	}
+
+	#HTML {
+	  padding-top: ${menu.offsetHeight}px!important;
+	}
+
+	#menu {
+	  position:fixed;
+	  top: 0;
+	  left: 0;
+	  right: 0;
+	}
+
+	#page_interstitiel{
+		display:none!important;
+		z-index:-999;
+	}
 	
-$('#page_top').remove();
-//$('#page_annonces_top').remove();
-$('#page').css({
-  'width': '100%'
-});
-$('#menu').css({
-  'position': 'fixed',
-  'top': 0,
-  'left': 0,
-  'z-index': 999999,
-  'width': '100%',
-  'padding-right': '20px'
-});
-$('#page_contenu').css({
-  padding: '20px',
-  width: '100%'
-});
-$('#page_middle').css({
-  width: '100%'
-});
-$('#footer').remove();
-$('#menu_contenu').css({
-  width: '100%'
-});
-$('#menu_sousmenu_annonces_categories').css({
-  display: 'none'
-});
+	#page_left {
+		padding-left: 0!important;
+		padding-right: 0!important;
+	}
 
-if (document.location.href == 'http://www.ouedkniss.com/') // si page == http://www.ouedkniss.com/
-$('#menu_annonces_a').click(function () {
-  if ($('#menu_sousmenu_annonces_categories').css('display') == 'none')
-  $('#menu_sousmenu_annonces_categories').css('display', 'block');
-   else
-  $('#menu_sousmenu_annonces_categories').css('display', 'none');
-});
+	`;
+}
 
-var removeIrames = function(){
+var remove_right = function () {
+	find("#page_right").forEach(e => e.remove());
+}
+
+var remove_frames = function () {
 	var tt = document.getElementsByTagName("iframe");
-	for(var i=0;i<tt.length;i++) 
-		if(tt[i].src.toString().indexOf("//www.ouedkniss.com/chat/chatbox.php") == -1) tt[i].remove();	
+	for (var i = 0; i < tt.length; i++)
+		if (tt[i].src.toString().indexOf("//www.ouedkniss.com/chat/chatbox.php") == -1)
+			tt[i].remove();
 }
 
-removeIrames();
+remove_frames();
 
-setInterval(removeIrames, 1000);
-  
-}
+add_style();
+
+remove_right();
+
+setInterval(remove_frames, 1000);
+
+})()
