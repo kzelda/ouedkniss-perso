9468fac18c885438601b4fa64bce01633bc31af7
kzelda <kzelda@users.noreply.github.com>
2021-11-23 09:21:03 +0100

    fix header

--- ouedkniss.css.user.js
+++ ouedkniss.css.user.js
@@ -4,72 +4,50 @@
 // @description Affichage css personalisé de ouedkniss.com
 // @include     http://www.ouedkniss.com/*
 // @include     https://www.ouedkniss.com/*
-// @version     1.0.0.1
+// @version     22.0.0
 // @grant       none
 // ==/UserScript==
 
 
 (function(){
 
-if (window.location.href.indexOf('/membre/') > -1) return;
 
-var body = document.querySelector("body");
-var menu = document.getElementById("menu");
-var find = (selector) => document.querySelectorAll(selector);
 
-var add_style = function () {
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+    args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate)
+        func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow)
+      func.apply(context, args);
+  };
+};
 
-	// create custom css style
-	var mystyle = document.createElement("style");
-	mystyle.id = "mystyle";
-	body.append(mystyle);
-	body.append(menu);
+var style = document.createElement("style");
 
-	mystyle.innerText = `
-
-	#banner {
-	 display: none
-	}
-
-	.body_banner {
-	  padding-top: 0!important;
-	}
-
-	#header {
-	  display:none
-	}
-
-	#topbar {
-	  display: none
-	}
-
-	#HTML {
-	  padding-top: ${menu.offsetHeight}px!important;
-	}
-
-	#menu {
-	  position:fixed;
-	  top: 0;
-	  left: 0;
-	  right: 0;
-	}
-
-	#page_interstitiel{
-		display:none!important;
-		z-index:-999;
-	}
-	
-	#page_left {
-		padding-left: 0!important;
-		padding-right: 0!important;
-	}
-
-	`;
+style.textContent = `
+#app header {
+  position: fixed !important;
+  width: 100%;
 }
+`;
 
-var remove_right = function () {
-	find("#page_right").forEach(e => e.remove());
-}
+document.body.appendChild(style);
+
+window.addEventListener("scroll", debounce(function () {
+
+    setTimeout(() => {
+      document.querySelector("header").attributes.style.value = "height: 56px; margin-top: 0px;left: 0px; right: 0px;";
+    }, 50)
+  }, 200));
 
 var remove_frames = function () {
 	var tt = document.getElementsByTagName("iframe");
@@ -79,11 +57,7 @@ var remove_frames = function () {
 }
 
 remove_frames();
-
-add_style();
-
-remove_right();
-
+	
 setInterval(remove_frames, 1000);
 
 })()
